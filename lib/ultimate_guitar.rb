6176c44b901e6307a3034cd32c5156f0ae36d065
Tal Safran <tsafran@gmail.com>
2012-11-29 14:25:20 -0500

    Log and rescue in the right places. Oops!

--- lib/ultimate_guitar.rb
+++ lib/ultimate_guitar.rb
@@ -5,16 +5,17 @@ Dir.glob(File.dirname(File.absolute_path(__FILE__)) + '/*/*') { |f| require f }
 
 module UltimateGuitar
   def fetch_with_regex(url, regex)
-    begin
-      fetch(url).css(regex, RegexLinkMatcher.new)
-    rescue Exception => e
-      Logger.log("Couldn't get #{url} - #{e.message}")
-      []
-    end
+    fetch(url).css(regex, RegexLinkMatcher.new)
   end
 
   def fetch(url)
     Logger.log("Fetching #{url}...")
-    Nokogiri::HTML(open(url))
+
+    begin
+      Nokogiri::HTML(open(url))
+    rescue Exception => e
+      Logger.log("Couldn't get #{url} - #{e.message}")
+      []
+    end
   end
 end
